928b1b1d08227e38ea0db8f0c1acd12bb6f92729
StastnyJ <xstastnyj@gmail.com>
2020-07-06 23:08:25 +0200

    basic message

--- src/Pages/Home/Components/Tipsport/MatchDetailRequest.tsx
+++ src/Pages/Home/Components/Tipsport/MatchDetailRequest.tsx
@@ -10,12 +10,25 @@ type propsType = {
   setSelectedEmail: Dispatch<SetStateAction<string>>;
 };
 
+enum categoryTypes {
+  ROOT = "ROOT",
+  CATEGORY = "CATEGORY",
+  SELECTION = "SELECTION",
+  SUPERSPORT = "SUPERSPORT",
+  SUPERGROUP = "SUPERGROUP",
+  SPORT = "SPORT",
+  GROUP = "GROUP",
+  COMPETITION = "COMPETITION",
+  MATCH = "MATCH",
+}
+
 export default function ({ newTipsportReq, setNewTipsportReq, emails, selectedEmail, setSelectedEmail }: propsType) {
   const [matches, setMatches] = useState<Match[]>([]);
+  const [category, setCategory] = useState<categoryTypes>(categoryTypes.COMPETITION);
 
   const loadMatches = (url: string) => {
     if (url.length >= 0) {
-      fetch(`/tipsport/v1/matches?url=${url}`)
+      fetch(`/tipsport/v1/matches?url=${url}&categoryType=${category}`)
         .then((response) => {
           if (response.ok) {
             response.json().then(setMatches);
@@ -29,6 +42,29 @@ export default function ({ newTipsportReq, setNewTipsportReq, emails, selectedEm
 
   return (
     <>
+      <br />
+      <br />
+      <FormControl style={{ width: "50%" }}>
+        <InputLabel>Category type</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value as categoryTypes);
+          }}
+        >
+          <MenuItem value={categoryTypes.ROOT}>Root</MenuItem>
+          <MenuItem value={categoryTypes.CATEGORY}>Category</MenuItem>
+          <MenuItem value={categoryTypes.SELECTION}>Selection</MenuItem>
+          <MenuItem value={categoryTypes.SUPERSPORT}>SuperSport</MenuItem>
+          <MenuItem value={categoryTypes.SUPERGROUP}>SuperGroup</MenuItem>
+          <MenuItem value={categoryTypes.SPORT}>Sport</MenuItem>
+          <MenuItem value={categoryTypes.GROUP}>Group</MenuItem>
+          <MenuItem value={categoryTypes.COMPETITION}>Competition</MenuItem>
+          <MenuItem value={categoryTypes.MATCH}>Match</MenuItem>
+        </Select>
+      </FormControl>
       <TextField label="Category URL" style={{ width: "100%" }} onBlur={(e) => loadMatches(e.target.value)} />
       {matches.length === 0 ? (
         <>
--- src/Pages/Home/Components/Tipsport/NewMatchesWatcher.tsx
+++ src/Pages/Home/Components/Tipsport/NewMatchesWatcher.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { TextField, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
 
 type propsType = {
@@ -9,17 +9,59 @@ type propsType = {
   setSelectedEmail: Dispatch<SetStateAction<string>>;
 };
 
+enum categoryTypes {
+  ROOT = "ROOT",
+  CATEGORY = "CATEGORY",
+  SELECTION = "SELECTION",
+  SUPERSPORT = "SUPERSPORT",
+  SUPERGROUP = "SUPERGROUP",
+  SPORT = "SPORT",
+  GROUP = "GROUP",
+  COMPETITION = "COMPETITION",
+  MATCH = "MATCH",
+}
+
 export default function ({ newTipsportReq, setNewTipsportReq, emails, selectedEmail, setSelectedEmail }: propsType) {
   if (newTipsportReq.apiUrl !== "/requests/v1/withStatus/")
     setNewTipsportReq({ ...newTipsportReq, apiUrl: "/requests/v1/withStatus/" });
 
+  const [category, setCategory] = useState<categoryTypes>(categoryTypes.COMPETITION);
+
   return (
     <>
+      <br />
+      <br />
+      <FormControl style={{ width: "50%" }}>
+        <InputLabel>Category type</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value as categoryTypes);
+          }}
+        >
+          <MenuItem value={categoryTypes.ROOT}>Root</MenuItem>
+          <MenuItem value={categoryTypes.CATEGORY}>Category</MenuItem>
+          <MenuItem value={categoryTypes.SELECTION}>Selection</MenuItem>
+          <MenuItem value={categoryTypes.SUPERSPORT}>SuperSport</MenuItem>
+          <MenuItem value={categoryTypes.SUPERGROUP}>SuperGroup</MenuItem>
+          <MenuItem value={categoryTypes.SPORT}>Sport</MenuItem>
+          <MenuItem value={categoryTypes.GROUP}>Group</MenuItem>
+          <MenuItem value={categoryTypes.COMPETITION}>Competition</MenuItem>
+          <MenuItem value={categoryTypes.MATCH}>Match</MenuItem>
+        </Select>
+      </FormControl>
       <TextField
         label="Category URL"
         value={newTipsportReq.url}
         onChange={(e) =>
-          setNewTipsportReq({ ...newTipsportReq, url: e.target.value, displayUrl: e.target.value, keyword: "Watchnig changes" })
+          setNewTipsportReq({
+            ...newTipsportReq,
+            url: e.target.value + "<categorySeparator>" + category,
+            displayUrl: e.target.value,
+            keyword: "Watchnig changes",
+          })
         }
         style={{ width: "100%" }}
       />
